f1def23ead09cb12e2ffa4923719684389a718d5
Bridget Phillips <jings.bill@gmail.com>
2026-07-08 15:54:31 -0700

    Report all sites of package name collisions

--- brevity-wit/src/jvmMain/kotlin/dev/wasmo/brevity/io/validation/validations.kt
+++ brevity-wit/src/jvmMain/kotlin/dev/wasmo/brevity/io/validation/validations.kt
@@ -1,36 +1,84 @@
 package dev.wasmo.brevity.io.validation
 
+import dev.wasmo.brevity.Offset
 import dev.wasmo.brevity.PackageName
-import dev.wasmo.brevity.WitException
+import dev.wasmo.brevity.WitCompoundException
+import dev.wasmo.brevity.WitMultiplySitedException
+import dev.wasmo.brevity.WitMultiplySitedException.Location
 import dev.wasmo.brevity.io.IoInlinePackage
 import dev.wasmo.brevity.io.IoToplevelWitPackage
+import dev.wasmo.brevity.io.IoWitFile
 import dev.wasmo.brevity.io.IoWitPackage
+import okio.Path
+import okio.Path.Companion.toPath
 
 fun validateUniquePackageNames(toplevelPackages: List<IoToplevelWitPackage>): Map<PackageName, IoWitPackage> {
-  val collisions = mutableMapOf<PackageName, MutableList<IoWitPackage>>()
-  return buildMap {
-    val inlinePackages = toplevelPackages.flatMap { toplevel ->
-      toplevel.files.values.flatMap { file ->
-        file.items.filterIsInstance<IoInlinePackage>()
-      }
-    }
-    for (pkg in toplevelPackages + inlinePackages) {
-      val existing = this[pkg.packageName]
-      if (existing != null) {
-        val collisionsList = collisions[pkg.packageName] ?: mutableListOf(existing).also {
-          collisions[pkg.packageName] = it
-        }
+  val packageRefs = mutableMapOf<PackageName, MutableList<PackageRef>>()
 
-        collisionsList.add(pkg)
-      } else {
-        this[pkg.packageName] = pkg
+  val addPackage: (PackageName, PackageRef)->Unit = { pkgName, pkg ->
+    packageRefs.getOrPut(pkgName) { mutableListOf() }.add(pkg)
+  }
+  for (pkg in toplevelPackages) {
+    addPackage(pkg.packageName, PackageRef.Directory(pkg))
+
+    for ((path, file) in pkg.files) {
+      for (inlinePkg in file.items.filterIsInstance<IoInlinePackage>()) {
+        addPackage(inlinePkg.packageName, PackageRef.Inline(
+          path, file, inlinePkg,
+        ))
       }
-    }
-    // Just blow up on the first collision for now.
-    collisions.entries.firstOrNull()?.let { (packageName, _) ->
-      throw WitException(
-        "Duplicate package definitions for $packageName",
-      )
     }
   }
+  val collisions = mutableMapOf<PackageName, List<PackageRef>>()
+  val output = mutableMapOf<PackageName, IoWitPackage>()
+
+  for ((packageName, packageRefs) in packageRefs) {
+    when (packageRefs.size) {
+      0 -> error("Invariant violated: package name exists without reference")
+      1 -> output[packageName] = packageRefs.single().pkg
+      else -> {
+        output[packageName] = packageRefs.first().pkg
+        collisions[packageName] = packageRefs
+      }
+    }
+  }
+
+  val collisionExceptions = collisions.map { (packageName, packageRefs) ->
+    WitMultiplySitedException("Duplicate definitions of $packageName", packageRefs.map { packageRef ->
+      Location(packageRef.path.toString(), packageRef.offset)
+    }.toList())
+  }
+
+  when (collisionExceptions.size) {
+    0 -> {}
+    1 -> throw collisionExceptions.single()
+    else -> throw WitCompoundException(collisionExceptions)
+  }
+
+  return output
 }
+
+sealed interface PackageRef {
+  val pkg: IoWitPackage
+  val path: Path
+  val offset: Offset
+
+  data class Directory(override val pkg: IoToplevelWitPackage): PackageRef {
+    private val firstPackageNameDef = pkg.files.firstNotNullOf { (path, file) ->
+      if (file.packageName != null) {
+        path
+      } else {
+        null
+      }
+    }
+
+    override val path = firstPackageNameDef
+    override val offset = Offset(0, 0)
+  }
+  data class Inline(override val path: Path, val file: IoWitFile, override val pkg: IoInlinePackage): PackageRef {
+    override val offset = pkg.offset
+  }
+}
+
+private val IoToplevelWitPackage.directory: Path
+  get() = files.keys.first().parent ?: "".toPath()
