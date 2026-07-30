b8190ad88f4a967853e80309f9b0b8d82cedf3e4
Bridget Phillips <jings.bill@gmail.com>
2026-07-30 10:58:41 -0700

    Make an actual API out of the PackageName and ServiceName maps

--- brevity-testing/src/jvmMain/kotlin/dev/wasmo/brevity/io/IrTesting.kt
+++ brevity-testing/src/jvmMain/kotlin/dev/wasmo/brevity/io/IrTesting.kt
@@ -1,12 +1,11 @@
 package dev.wasmo.brevity.io
 
-import dev.wasmo.brevity.IssueCollector
-import dev.wasmo.brevity.io.validation.validate
+import dev.wasmo.brevity.io.validation.buildSymbolTable
 import dev.wasmo.brevity.ir.IrMapper
 import dev.wasmo.brevity.withIssueCollector
 
 fun IrMapper(toplevelWitPackages: List<IoToplevelWitPackage>): IrMapper = withIssueCollector {
-  val validatedPackages = toplevelWitPackages.validate()
+  val symbolTable = toplevelWitPackages.buildSymbolTable()
   throwIfNotEmpty()
-  IrMapper(validatedPackages!!)
+  IrMapper(toplevelWitPackages, symbolTable!!)
 }
