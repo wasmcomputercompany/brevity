5ecb531ba88593dc37068fe696b11d304779ab30
Bridget Phillips <jings.bill@gmail.com>
2026-09-04 13:52:46 -0700

    Rework IssueCollector API surface

--- brevity-wit/src/jvmMain/kotlin/dev/wasmo/brevity/IssueCollector.kt
+++ brevity-wit/src/jvmMain/kotlin/dev/wasmo/brevity/IssueCollector.kt
@@ -7,68 +7,130 @@ import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
-class IssueCollector private constructor(
-  issues: MutableList<Issue>,
-  val locationStack: List<Location>,
-) {
-  constructor(): this(issues = mutableListOf(), locationStack = emptyList())
+/**
+ * Manages a shared set of issues, and provides interfaces for scoped collection of issues
+ */
+interface IssueRegistry {
+  val all: List<Issue>
 
-  private val _issues = issues
-  val issues: List<Issue> = _issues
-
-  fun report(issue: Issue) {
-    _issues += issue.copy(locationStack = locationStack)
-  }
-
-  fun throwIfNotEmpty() {
-    if (issues.isNotEmpty()) {
-      throw WitCompoundException(
-        issues.map { WitException(it) }
-      )
-    }
-  }
-
-  internal fun pushIssueLocation(location: Location)
-    = IssueCollector(_issues, listOf(location).plus(locationStack))
+  /**
+   * A sequence of contextual locations, ordered from innermost to outermost.
+   */
+  val locationStack: List<Location>
 }
 
-context(issueCollector: IssueCollector)
-fun <T> pushIssueLocation(location: Location, block: IssueCollector.() -> T): T {
-  contract {
-    callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-  }
-  val issueCollector = issueCollector.pushIssueLocation(location)
-  return issueCollector.block()
+interface IssueCollector : IssueRegistry {
+  fun report(issue: Issue)
 }
 
 /**
- * Convenience function to run a block of code and throw if any issues are raised by it.
+ * Singleton instance used to introduce [issues] into the namespace
  */
-fun <T> collectNoIssuesOrThrow(block: IssueCollector.()->T): T {
+object IssueReceiver {
+  context(issueCollector: IssueCollector)
+  val issues: IssueCollector
+    get() = issueCollector
+}
+
+/**
+ * Constructs a fresh [IssueRegistry] with an empty issue list
+ */
+fun issues(): IssueRegistry = RealIssueCollector(
+  _all = mutableListOf(),
+  _scoped = mutableListOf(),
+  locationStack = emptyList(),
+  parent = null,
+)
+
+private class RealIssueCollector(
+  val _all: MutableList<Issue>,
+  val _scoped: MutableList<Issue>,
+  override val locationStack: List<Location>,
+  val parent: RealIssueCollector?,
+) : IssueCollector {
+  override val all: List<Issue> = _all
+
+  val scoped: List<Issue> = _scoped
+
+  override fun report(issue: Issue) {
+    val localized = issue.copy(
+      locationStack = issue.locationStack + locationStack,
+    )
+    _all += localized
+    _scoped += localized
+  }
+
+  fun newChild(
+    scoped: MutableList<Issue> = mutableListOf(),
+    locationStack: List<Location> = this.locationStack,
+  ) = RealIssueCollector(
+    _all = _all,
+    _scoped = scoped,
+    locationStack = locationStack,
+    parent = this,
+  )
+}
+
+/**
+ * If an issue is thrown within [block], upon completion [collectNoneOrThrow] will throw
+ * an exception containing all issues.
+ */
+fun <T> IssueRegistry.collectNoneOrThrow(block: context(IssueCollector) IssueReceiver.() -> T): T {
   contract {
     callsInPlace(block, InvocationKind.EXACTLY_ONCE)
   }
-  val issueCollector = IssueCollector()
+  require(this is RealIssueCollector) // only implementation
+
+  val issueCollector = newChild()
   val result = with(issueCollector) {
-    block()
+    IssueReceiver.block()
   }
 
-  issueCollector.throwIfNotEmpty()
+  if (all.isNotEmpty()) {
+    throw WitCompoundException(
+      all.map { WitException(it) }
+    )
+  }
 
   return result
 }
 
 /**
- * Run a block, returning the result and any issues raised.
+ * Collect issues within the given scope and return them, along with a return value
  */
-fun <T> collectIssues(block: IssueCollector.()->T): Pair<T, List<Issue>> {
+fun <T> IssueRegistry.collect(block: context(IssueCollector) IssueReceiver.()->T): Pair<T, List<Issue>> {
   contract {
     callsInPlace(block, InvocationKind.EXACTLY_ONCE)
   }
-  val issueCollector = IssueCollector()
+  require(this is RealIssueCollector) // only implementation
+
+  val issueCollector = newChild()
   val result = with(issueCollector) {
-    block()
+    IssueReceiver.block()
   }
 
-  return result to issueCollector.issues
+  return result to issueCollector.scoped
+}
+
+/**
+ * Push a location onto the front of [locationStack]
+ *
+ * Issues reported here are reported into the enclosing scope from [collectNoneOrThrow] or
+ * [collect].
+ */
+fun <T> IssueRegistry.push(location: Location, block: context(IssueRegistry) IssueReceiver.() -> T): T {
+  contract {
+    callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+  }
+  require(this is RealIssueCollector) // only implementation
+
+  val issueCollector = newChild(
+    scoped = _scoped,
+    locationStack = listOf(location) + locationStack,
+  )
+  val result = with(issueCollector) {
+    IssueReceiver.block()
+  }
+
+  return result
 }
