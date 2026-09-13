33f826b8aa1edb3098148c25651426408810b6d7
Jesse Wilson <jesse@wasmo.com>
2026-09-13 22:43:47 +0300

    Simplify creating Kotlin names (#162)
    The original design predates our IR models, so it was designed
    to build names recursively. We don't need this anymore.

--- brevity-kotlin-generator/src/jvmTest/kotlin/dev/wasmo/brevity/kotlin/generator/KotlinNameTest.kt
+++ brevity-kotlin-generator/src/jvmTest/kotlin/dev/wasmo/brevity/kotlin/generator/KotlinNameTest.kt
@@ -3,32 +3,48 @@ package dev.wasmo.brevity.kotlin.generator
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import com.squareup.kotlinpoet.ClassName
-import dev.wasmo.brevity.Identifier
+import dev.wasmo.brevity.io.toServiceName
+import dev.wasmo.brevity.ir.TypeNameDeclared
 import dev.wasmo.brevity.toPackageName
 import kotlin.test.Test
 
 class KotlinNameTest {
   @Test
   fun `package name mapping`() {
-    val value = "wasi:clocks".toPackageName().toKotlin()
-    assertThat(value.name).isEqualTo("wit.wasi.clocks")
-    assertThat((value + Identifier("wall-clock")).name)
+    val packageName = "wasi:clocks".toPackageName()
+    val serviceName = "wasi:clocks/wall-clock".toServiceName()
+    val typeName = TypeNameDeclared("wasi:clocks/wall-clock", "datetime")
+    assertThat(packageName.kotlinApi)
+      .isEqualTo("wit.wasi.clocks")
+    assertThat(serviceName.kotlinApi)
       .isEqualTo(ClassName("wit.wasi.clocks", "WallClock"))
+    assertThat(typeName.kotlinApi)
+      .isEqualTo(ClassName("wit.wasi.clocks", "WallClock", "Datetime"))
   }
 
   @Test
   fun `package name mapping with version`() {
-    val value = "wasi:clocks@0.2.12".toPackageName().toKotlin()
-    assertThat(value.name).isEqualTo("wit.wasi.clocks.v0_2_12")
-    assertThat((value + Identifier("wall-clock")).name)
+    val packageName = "wasi:clocks@0.2.12".toPackageName()
+    val serviceName = "wasi:clocks/wall-clock@0.2.12".toServiceName()
+    val typeName = TypeNameDeclared("wasi:clocks/wall-clock@0.2.12", "datetime")
+    assertThat(packageName.kotlinApi)
+      .isEqualTo("wit.wasi.clocks.v0_2_12")
+    assertThat(serviceName.kotlinApi)
       .isEqualTo(ClassName("wit.wasi.clocks.v0_2_12", "WallClock"))
+    assertThat(typeName.kotlinApi)
+      .isEqualTo(ClassName("wit.wasi.clocks.v0_2_12", "WallClock", "Datetime"))
   }
 
   @Test
   fun `package name mapping with dashes`() {
-    val value = "wasi:grandfather-clocks".toPackageName().toKotlin()
-    assertThat(value.name).isEqualTo("wit.wasi.grandfatherclocks")
-    assertThat((value + Identifier("wall-clock")).name)
+    val packageName = "wasi:grandfather-clocks".toPackageName()
+    val serviceName = "wasi:grandfather-clocks/wall-clock".toServiceName()
+    val typeName = TypeNameDeclared("wasi:grandfather-clocks/wall-clock", "datetime")
+    assertThat(packageName.kotlinApi)
+      .isEqualTo("wit.wasi.grandfatherclocks")
+    assertThat(serviceName.kotlinApi)
       .isEqualTo(ClassName("wit.wasi.grandfatherclocks", "WallClock"))
+    assertThat(typeName.kotlinApi)
+      .isEqualTo(ClassName("wit.wasi.grandfatherclocks", "WallClock", "Datetime"))
   }
 }
