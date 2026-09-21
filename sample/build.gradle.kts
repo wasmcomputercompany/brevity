beaaf585a4c57aa88b68e419bc01c1e0ff6dbb53
Bridget Phillips <jings.bill@gmail.com>
2026-09-21 11:10:24 -0700

    Integrate wkg fetch into build

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -6,15 +6,6 @@ plugins {
 
 
 brevity {
-  ociPackages.addAll(
-    "wasi:cli@0.2.0",
-    "wasi:clocks@0.2.0",
-    "wasi:filesystem@0.2.0",
-    "wasi:http@0.2.0",
-    "wasi:io@0.2.0",
-    "wasi:random@0.2.0",
-    "wasi:sockets@0.2.0",
-  )
   customTypeMappings.put("wasmo:uuid/types.uuid@0.1.0", "kotlin.uuid.Uuid")
   customTypeMappings.put("wasi:clocks/wall-clock.datetime@0.2.0", "kotlin.time.Instant")
   worlds.add("wasmo:platform/wasmo")
